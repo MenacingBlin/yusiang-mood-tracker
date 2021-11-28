b3a2ba251166eaead2a65cc70d4a3fbc62785093
MenacingBlin <thepoliwrathgaming@gmail.com>
2021-11-28 08:14:59 +0000

    da

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,23 +5,25 @@
 </script>
 
 <Greeting />
-<!--   Entries   -->
+<!-- Entries -->
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input
-   		 class="btn btn-light mb-2"
-   		 type="button"
-   		 value="+ New Entry"
-   		 data-bs-toggle="modal"
-   		 data-bs-target="#newEntry"
-   	 />
+   	 <input 
+        class="btn btn-light mb-2" 
+        type="button" 
+        value="+ New Entry" 
+        data-bs-toggle="modal"
+        data-bs-target="#newEntry"
+        />
     </div>
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
-   	 <Entry />
+    	<Entry />
+   	 
     </div>
 </section>
 
 <EntryModal />
+
